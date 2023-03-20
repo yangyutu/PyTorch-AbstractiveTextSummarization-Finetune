74fce17bcef6b965e2938c24dedfd46bbb7ac8f0
Yuguang Yang <yangyutu123@gmail.com>
2023-03-20 09:37:34 -0700

    add BRIO implementation

--- data_utils.py
+++ data_utils.py
@@ -170,6 +170,30 @@ def collate_finetune(batch, pad_token_id, is_test=False):
         result["data"] = data
     return result
 
+def collate_finetune_with_candidates(batch, pad_token_id, is_test=False):
+    def pad(X, max_len=-1):
+        if max_len < 0:
+            max_len = max(x.size(0) for x in X)
+        result = torch.ones(len(X), max_len, dtype=X[0].dtype) * pad_token_id
+        for (i, x) in enumerate(X):
+            result[i, : x.size(0)] = x
+        return result
+
+    src_input_ids = pad([x["src_input_ids"] for x in batch])
+    candidate_ids = [x["candidate_ids"] for x in batch]
+    max_len = max([max([len(c) for c in x]) for x in candidate_ids])
+    candidate_ids = [pad(x, max_len) for x in candidate_ids]
+    candidate_ids = torch.stack(candidate_ids)
+    if is_test:
+        data = [x["data"] for x in batch]
+    result = {
+        "src_input_ids": src_input_ids,
+        "candidate_ids": candidate_ids,
+    }
+    if is_test:
+        result["data"] = data
+    return result
+
 
 def collate_clm_finetune(batch, pad_token_id=-100, is_test=False):
     # This collate function is mainly used for padding
@@ -194,6 +218,104 @@ def collate_clm_finetune(batch, pad_token_id=-100, is_test=False):
     return result
 
 
+class CNNDailyWithCandidatesSeq2SeqDataset(Dataset):
+    def __init__(
+        self,
+        fdir,
+        tokenizer_name,
+        is_test=False,
+        summary_max_len=-1,
+        article_max_len=512,
+        is_sorted=True,
+        max_cand_num=-1,
+        is_untok=True,
+        num=-1,
+    ):
+        """data format: article, abstract, [(candidiate_i, score_i)]"""
+        self.isdir = os.path.isdir(fdir)
+        if self.isdir:
+            self.fdir = fdir
+            if num > 0:
+                self.num = min(len(os.listdir(fdir)), num)
+            else:
+                self.num = len(os.listdir(fdir))
+        else:
+            with open(fdir) as f:
+                self.files = [x.strip() for x in f]
+            if num > 0:
+                self.num = min(len(self.files), num)
+            else:
+                self.num = len(self.files)
+        self.tok = AutoTokenizer.from_pretrained(
+            tokenizer_name, verbose=False, model_max_length=512
+        )
+        self.summary_max_len = summary_max_len
+        self.is_test = is_test
+        self.article_max_len = article_max_len
+        self.sorted = is_sorted
+        self.max_cand_num = max_cand_num
+        self.is_untok = is_untok
+
+    def __len__(self):
+        return self.num
+
+    def __getitem__(self, idx):
+        if self.isdir:
+            with open(os.path.join(self.fdir, "%d.json" % idx), "r") as f:
+                data = json.load(f)
+        else:
+            with open(self.files[idx]) as f:
+                data = json.load(f)
+        if self.is_untok:
+            article = data["article_untok"]
+        else:
+            article = data["article"]
+        src_txt = " ".join(article)
+        src = self.tok.batch_encode_plus(
+            [src_txt],
+            max_length=self.article_max_len,
+            return_tensors="pt",
+            pad_to_max_length=False,
+            truncation=True,
+        )
+        src_input_ids = src["input_ids"]
+        src_input_ids = src_input_ids.squeeze(0)
+        if self.is_untok:
+            abstract = data["abstract_untok"]
+        else:
+            abstract = data["abstract"]
+        if self.max_cand_num > 0:
+            candidates = data["candidates_untok"][: self.max_cand_num]
+            _candidates = data["candidates"][: self.max_cand_num]
+            data["candidates"] = _candidates
+        if self.sorted:
+            candidates = sorted(candidates, key=lambda x: x[1], reverse=True)
+            _candidates = sorted(_candidates, key=lambda x: x[1], reverse=True)
+            data["candidates"] = _candidates
+        if not self.is_untok:
+            candidates = _candidates
+        
+        # cand_txt consists of the gold reference plus other candidates
+        cand_txt = [" ".join(abstract)] + [" ".join(x[0]) for x in candidates]
+        cand = self.tok.batch_encode_plus(
+            cand_txt,
+            max_length=self.summary_max_len,
+            return_tensors="pt",
+            pad_to_max_length=False,
+            truncation=True,
+            padding=True,
+        )
+        candidate_ids = cand["input_ids"]
+
+        result = {
+            "src_input_ids": src_input_ids,
+            "candidate_ids": candidate_ids,
+        }
+        if self.is_test:
+            result["data"] = data
+        return result
+
+
 def _test_finetune_seq2seq_data():
     data_set = CNNDailySeq2SeqDataset(
         "t5-large",
@@ -202,9 +324,6 @@ def _test_finetune_seq2seq_data():
         article_max_len=1024,
         model_type="t5",
     )
-
-    # for i in range(10):
-    #     print(data_set[i])
 
     tok = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
     tok = AutoTokenizer.from_pretrained("t5-large")
@@ -217,6 +336,36 @@ def _test_finetune_seq2seq_data():
 
     dataloader = DataLoader(
         data_set, batch_size=2, shuffle=False, num_workers=4, collate_fn=collate_fn
+    )
+
+    for batch in dataloader:
+        print(batch)
+        break
+
+
+def _test_finetune_seq2seq_with_candidates_data():
+    model_name = "facebook/bart-large-cnn"
+    fdir = "/mnt/d/MLData/data/summarization/cnndm_bart/cnndm/diverse/test/"
+    data_set = CNNDailyWithCandidatesSeq2SeqDataset(
+        fdir=fdir,
+        tokenizer_name=model_name,
+        is_test=True,
+        summary_max_len=128,
+        article_max_len=512,
+        max_cand_num=16
+    )
+
+    print(data_set[0])
+    tok = AutoTokenizer.from_pretrained(model_name)
+
+    collate_fn = partial(
+        collate_finetune_with_candidates,
+        pad_token_id=tok.pad_token_id,
+        is_test=True,
+    )
+
+    dataloader = DataLoader(
+        data_set, batch_size=1, shuffle=False, num_workers=4, collate_fn=collate_fn
     )
 
     for batch in dataloader:
@@ -249,5 +398,6 @@ def _test_finetune_clm_data():
 
 
 if __name__ == "__main__":
-    _test_finetune_clm_data()
-    _test_finetune_seq2seq_data()
+    # _test_finetune_clm_data()
+    # _test_finetune_seq2seq_data()
+    _test_finetune_seq2seq_with_candidates_data()
--- finetune_model.py
+++ finetune_model.py
@@ -7,7 +7,7 @@ from transformers import BartForConditionalGeneration, BartTokenizer
 from label_smoothing_loss import label_smoothing_loss
 from typing import Dict
 import pytorch_lightning as pl
-
+from losses import RankingLoss
 
 class FinetuneSeq2SeqModel(pl.LightningModule):
     def __init__(self, config: Dict, pretrained_model, pad_token_id, label_smooth=0.0):
@@ -104,6 +104,133 @@ class FinetuneSeq2SeqModel(pl.LightningModule):
                 **kwargs,
             )
         return outputs
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), self.config["lr"])
+        lr_warmup_steps = self.config["lr_warm_up_steps"]
+
+        def lr_foo(step):
+            lr_scale = min((step + 1) ** (-0.5), step / lr_warmup_steps ** (1.5))
+
+            return lr_scale
+
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_foo)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "interval": "step",
+                "frequency": 1,
+            },
+        }
+
+class FinetuneWithContrastSeq2SeqModel(pl.LightningModule):
+    def __init__(self, config: Dict, pretrained_model, pad_token_id, label_smooth=0.0):
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = pretrained_model
+        self.pad_token_id = pad_token_id
+        self.config = config
+        self.adding = self.config.get('adding',0.0)
+        self.length_penalty = self.config.get('length_penalty',2.0)
+        self.mle_fn = nn.CrossEntropyLoss(ignore_index=pad_token_id)
+        self.ranking_loss = RankingLoss
+
+        self.margin, self.gold_margin, self.gold_weight = 0.0, 0.0, 0.001
+        self.ctr_loss_weight, self.mle_weight = 1.0, 1.0
+
+    def forward(self, text_id, target_id):
+
+        batch_size = text_id.size(0)
+        
+        new_text_id = []
+        for _ in range(target_id.size(0)):
+            new_text_id.append(text_id.expand(target_id.size(1), -1))
+        
+        text_id = torch.concat(new_text_id)
+        target_id = target_id.reshape(-1, target_id.size(-1))
+
+        input_mask = text_id != self.pad_token_id
+        target_mask = target_id != self.pad_token_id
+        # cand_mask[:, :, 0] = 1
+        output = self.model(
+            input_ids=text_id,
+            attention_mask=input_mask,
+            decoder_input_ids=target_id,
+            decoder_attention_mask=target_mask,
+            output_hidden_states=True,
+            return_dict=True,
+        )
+
+        logits = output["logits"]  # [bz x cand_num, seq_len, word_dim]
+        return logits
+
+    def _compute_loss(self, batch):
+        
+        logits_raw = self.forward(batch["src_input_ids"], batch["candidate_ids"])
+        batch_size = batch["src_input_ids"].size(0)
+        logits_raw = logits_raw.view(
+            batch_size, -1, logits_raw.size(1), logits_raw.size(2)
+        )  # [bz, cand_num, seq_len, word_dim] # 17 cand, 1 is gold, other are the generated candidates
+        
+        logits_raw = logits_raw[:, :, :-1]  # truncate last token # 1 x cand x (seq_len - 1) x word_dim
+        gold_logits = logits_raw[:, 0] # logits_raw for the gold # 1 x seq_len x word_dim
+        candidate_id = batch["candidate_ids"][:, :, 1:]  # shift right
+        cand_mask = candidate_id != self.pad_token_id
+        candidate_id = candidate_id.unsqueeze(-1)
+
+        logits_raw_normalized = F.log_softmax(logits_raw, dim=3)
+        logits_at_candidate_tokens = torch.gather(logits_raw_normalized, 3, candidate_id).squeeze(-1)  # [bz, cand_num, seq_len]
+
+        cand_mask = cand_mask.float()
+        scores = torch.mul(logits_at_candidate_tokens, cand_mask).sum(-1) / (
+            (cand_mask.sum(-1) + self.adding) ** self.length_penalty
+        )  # [bz, cand_num] ## score here is the sum of log prob of tokens in different steps
+
+        candiate_score = scores[:, 1:]
+        gold_score = scores[:, 0]
+
+        gold_ids = batch["candidate_ids"][:, 0, 1:]  # shift right # the first one is the gold reference
+            
+        mle_loss = self.mle_fn(gold_logits.view(-1, gold_logits.size(-1)), gold_ids.view(-1)) # self.mle_fn(gold_logits, gold_ids)
+            
+        ctr_loss = self.ranking_loss(candiate_score, gold_score, self.margin, self.gold_margin, self.gold_weight)
+            
+        loss = self.ctr_loss_weight * ctr_loss + self.mle_weight * mle_loss
+
+
+        return loss, mle_loss, ctr_loss, batch_size
+
+    def training_step(self, batch, batch_idx=0):
+
+        loss, mle_loss, ctr_loss, batch_size = self._compute_loss(batch)
+        self.log_dict(
+            {'ctr_loss': ctr_loss,
+             'mle_loss': mle_loss,
+              'loss':loss  
+            },
+            batch_size=batch_size,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+        return loss
+
+    def validation_step(self, batch, batch_idx=0):
+
+        loss, mle_loss, ctr_loss, batch_size = self._compute_loss(batch)
+        self.log_dict(
+            {'ctr_loss': ctr_loss,
+             'mle_loss': mle_loss,
+              'val_loss':loss  
+            },
+            batch_size=batch_size,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), self.config["lr"])
@@ -258,7 +385,54 @@ def _test_seq2seq_model_finetune():
         loss = model.training_step(batch)
         break
 
+def _test_seq2seq_with_candidates_model_finetune():
+    from torch.utils.data import DataLoader
+    from transformers import (
+        BartForConditionalGeneration,
+        AutoTokenizer,
+    )
+    from functools import partial
+    from data_utils import CNNDailyWithCandidatesSeq2SeqDataset, collate_finetune_with_candidates
 
+    model_name = "facebook/bart-large-cnn"
+    fdir = "/mnt/d/MLData/data/summarization/cnndm_bart/cnndm/diverse/test/"
+    data_set = CNNDailyWithCandidatesSeq2SeqDataset(
+        fdir=fdir,
+        tokenizer_name=model_name,
+        is_test=True,
+        summary_max_len=128,
+        article_max_len=512,
+        max_cand_num=16
+    )
+
+    tok = AutoTokenizer.from_pretrained(model_name)
+
+    collate_fn = partial(
+        collate_finetune_with_candidates,
+        pad_token_id=tok.pad_token_id,
+        is_test=True,
+    )
+
+    dataloader = DataLoader(
+        data_set, batch_size=1, shuffle=False, num_workers=4, collate_fn=collate_fn
+    )
+    tok = AutoTokenizer.from_pretrained(model_name)
+
+    config = {}
+    config["lr"] = 2e-3
+    config["lr_warm_up_steps"] = 10000
+
+    pretrained_model = BartForConditionalGeneration.from_pretrained(
+        model_name, cache_dir="./local_cache"
+    )
+    model = FinetuneWithContrastSeq2SeqModel(config, pretrained_model, tok.pad_token_id, 0.0)
+
+    count = 0
+    for batch in dataloader:
+        # print(batch)
+        loss = model.training_step(batch)
+        break
+    
 def _test_clm_model_finetune():
     from torch.utils.data import Dataset, DataLoader
     from transformers import (
@@ -309,5 +483,6 @@ def _test_clm_model_finetune():
 
 if __name__ == "__main__":
     # _test_BART_finetune()
-    _test_seq2seq_model_finetune()
-    _test_clm_model_finetune()
+    # _test_seq2seq_model_finetune()
+    # _test_clm_model_finetune()
+    _test_seq2seq_with_candidates_model_finetune()
